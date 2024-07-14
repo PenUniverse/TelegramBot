f993629310c1f7496ee9e648638c8796e5a73cd7
Redbeanw44602 <redbeana44945@gmail.com>
2024-07-15 01:52:25 +0800

    update.

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ async def main():
     branch = os.environ.get('BRANCH')
     repository = os.environ.get('REPOSITORY').split('/')[1]
     commits = json.loads(os.environ.get('COMMITS'))
-    msg = f'🔨 **{len(commits)}** new commit(s) to **{repository}:{branch}**\n\n'
+    msg = f'🔨 *{len(commits)}* new commit(s) to *{repository}:{branch}*\n\n'
     for commit in commits:
-        msg += f'*{commit['id'][:7]}*  {commit['message']}\n'
+        msg += f'*{commit['id'][:7]}* {commit['message']}\n'
     msg = msg.removesuffix('\n')
     await bot.send_message(chat_id=GROUP_ID, text=msg, message_thread_id=THREAD_ID, parse_mode=ParseMode.MARKDOWN)
 
